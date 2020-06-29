7ba2af39bca94c7283f7e8b76cc6c4dff6ae40dd
Kabiswa Davis <kabiswad@gmail.com>
2020-06-29 22:50:04 +0200

    top sectio

--- src/components/PageTop.jsx
+++ src/components/PageTop.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import me from '../images/me.jpg';
 
 function PageTop() {
   return (
     <div className="page-top">
-        <div className="top-content">
-            <div className="top-words">
-                {/* <h1>Hey, I'm Kabiswa Davis</h1> */}
-            </div>
-            <div className="top-image">
-
+        <div className="top-content container">
+            <div className="row">
+                <div className="col-md-8 top-words">
+                    <h1>Hey, I'm Kabiswa Davis.</h1>
+                    <p>A software developer, specilised in full stack web development, you have a project you want to work on lets talk</p>
+                </div>
+                <div className="col-md-4 top-image">
+                    <img src={me} alt="Davis" />
+                </div>
             </div>
         </div>
     </div>
