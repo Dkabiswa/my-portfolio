9eaf7df15cb925b44f17dc5c54605a327195abc1
Kabiswa Davis <kabiswad@gmail.com>
2020-06-30 04:55:07 +0200

    about section

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container about-me">
+            <h1>Who I Am?</h1>
+            <p>I'm Full-Stack Software Developer holding a bachelor's degree in Computer Science .Am currently available with most times focusing on free lance and personal projects.
+                I have invested time in mastering my craft and my freqauntly used skills include.
+                <ul>
+                    <li>React</li>
+                    <li>Angular</li>
+                    <li>Vue</li>
+                    <li>Node/Express</li>
+                    <li>TypeScript, JavaScript</li>
+                    <li>Docker</li>
+                    <li>Pythoon</li>
+                </ul>
+                My current personal project is <a href="https://github.com/Dkabiswa/Insta-Media">InstaMedia</a> an application which elps you embed instagram wall
+                in you react and vue websites.
+            </p>
+            <p>When not coding i enjoy listening to wide range of music from reggea to OPM, Watch Football a devoted manchester united fan
+                Read books and tech blogs.
+            </p>
+            <h1>Why work with me?</h1>
+            <p>I enjoy offering solutions to user challenges and believe we can change the world one line of code at a time
+                I respect deadlines and takecommunication as key.
+            </p>
+        </div>
+    )
+}
+
+export default About;
